ee1576827dd22f17b03b01967ae900016bfc88af
Wise Dev <86822394+wisedev99@users.noreply.github.com>
2022-08-04 14:37:12 +0500

    test commit for check deploy

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center justify-center h-full space-y-4 text-xl">
-    <p>Here's a counter</p>
+    <p>I'm Aliakbar</p>
     <div class="flex items-center justify-center">
 
     </div>
