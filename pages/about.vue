976a8a34880029e8c9d47e1cb0a5423f37843212
Wise Dev <86822394+wisedev99@users.noreply.github.com>
2022-08-05 15:08:19 +0500

    design home page for phone 360

--- pages/about.vue
+++ pages/about.vue
@@ -1,19 +1,95 @@
 <template>
   <div class="flex flex-col justify-center w-full h-screen max-w-3xl mx-auto space-y-2">
-    <div>
-      <h1 class="text-2xl bg-blue text-red">About</h1>
-      <p class="text-lg">I'm learning about Nuxt 3! Here's what I've achieved so far:</p>
+      <div class="container">
+        <pre class="center" id="d">{{ pretag }}</pre>
     </div>
-    <ul class="mx-8 list-disc">
-      <li>
-        Created a new Nuxt 3 project
-      </li>
-      <li>
-        Added Tailwind
-      </li>
-      <li>
-        Created a new page
-      </li>
-    </ul>
   </div>
 </template>
+
+<script setup>
+
+const pretag = ref('wdwqdw');
+
+var tmr1 = undefined,
+  tmr2 = undefined;
+var A = 1,
+  B = 1;
+
+var asciiframe = () => {
+  var b = [];
+  var d = [];
+  var z = [];
+  A += 0.07;
+  B += 0.03;
+  var cA = Math.cos(A),
+    sA = Math.sin(A),
+    cB = Math.cos(B),
+    sB = Math.sin(B);
+  for (var k = 0; k < 1760; k++) {
+    b[k] = k % 80 == 79 ? "\n" : " ";
+    z[k] = 0;
+  }
+  for (var j = 0; j < 6.28; j += 0.07) {
+    // j <=> theta
+    var ct = Math.cos(j),
+      st = Math.sin(j);
+    for (var i = 0; i < 6.28; i += 0.02) {
+      // i <=> phi
+      var sp = Math.sin(i),
+        cp = Math.cos(i),
+        h = ct + 2, // R1 + R2*cos(theta)
+        D = 1 / (sp * h * sA + st * cA + 5), // this is 1/z
+        t = sp * h * cA - st * sA; // this is a clever factoring of some of the terms in x' and y'
+
+      var x = 0 | (40 + 30 * D * (cp * h * cB - t * sB)),
+        y = 0 | (12 + 15 * D * (cp * h * sB + t * cB)),
+        o = x + 80 * y,
+        N =
+          0 |
+          (8 *
+            ((st * sA - sp * ct * cA) * cB -
+              sp * ct * sA -
+              st * cA -
+              cp * ct * sB));
+      if (y < 22 && y >= 0 && x >= 0 && x < 79 && D > z[o]) {
+        z[o] = D;
+        b[o] = ".,-~:;=!*#$@ "[N > 0 ? N : 0];
+
+      }
+    }
+  }
+  pretag.value = b.join("");
+};
+
+setInterval(asciiframe, 50)
+
+</script>
+  <style>
+        ::selection {
+            background: #505050;
+        }
+
+        ::-moz-selection {
+            background: #505050;
+        }
+
+
+        .container {
+            overflow: hidden;
+            background-color: black;
+            height: 100%;
+        }
+
+        .container {
+            color: white;
+            display: -webkit-flexbox;
+            display: -ms-flexbox;
+            display: -webkit-flex;
+            display: flex;
+            -webkit-flex-align: center;
+            -ms-flex-align: center;
+            -webkit-align-items: center;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,51 @@
 <template>
-  <div class="flex flex-col items-center justify-center h-full space-y-4 text-xl">
-    <p>I'm Aliakbar</p>
-    <div class="flex items-center justify-center">
+	<div
+		class="flex flex-col items-center justify-center h-full space-y-4 text-xl"
+	>
+		<div class="w-full h-full">
+			<div
+				class="
+					w-96
+					absolute
+					z-50
+					flex flex-col
+					justify-center
+					h-auto
+					p-4
+					space-y-2
+				"
+			>
+				<h1 class="text-4xl">Восстановление детей и взрослых</h1>
+				<p
+					class="text-2xl text-green-500 font-serif"
+					style="font-family: cursive"
+				>
+					с детским церебральным параличом
+				</p>
+			</div>
 
-    </div>
-  </div>
+			<div class="w-full h-full absolute object-left-bottom left-11">
+				<img
+					:src="'../assets/images/Framesss.svg'"
+					class="w-full h-full"
+					alt=""
+				/>
+				<div class="flex absolute z-75 top-36 w-4/5 h-auto space-y-0">
+					<img :src="'../assets/images/Group15.svg'" alt="" />
+					<img
+						:src="'../assets/images/Groupbag.svg'"
+						style="margin-top: 122px"
+						alt=""
+					/>
+				</div>
+			</div>
+				<div class=" w-full m-12 mr-12 h-2/3
+          p-4 bg-blue-600">
+
+        </div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-
 </script>
